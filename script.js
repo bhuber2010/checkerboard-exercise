927f2702c98de68aff90e15f7a6a2f0287d03ffe
bhuber2010 <bhuber@alumni.colostate.edu>
2015-11-03 19:43:04 -0700

    colors go trippy

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var body = document.getElementsByTagName('body')[0];
-body.style.margin = "0";
+document.getElementsByTagName('body')[0].style.margin = "0";
 
 var i = 0;
 while (i<99) {
@@ -12,4 +11,12 @@ while (i<99) {
   tile.style.backgroundColor = ranColor;
   document.body.appendChild(tile);
   i++;
-}
+};
+
+setInterval(function() {
+  var d = 0;
+  while(d<99){
+  document.querySelectorAll('div')[d].style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  d++;
+  }
+}, 200);
